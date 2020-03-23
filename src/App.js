d6d34aa714d945d4d5e9a24e985f6f46dee17026
alekoxo <aleko@incendiaryblue.com>
2020-03-23 14:40:41 -0700

    addes styled-components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+// import styled from 'styled-components';
+
+// const StyledButton = styled.button`
+//   background-color: ${props => props.alt ? 'red' : 'green'};
+//   color: white;
+//   font: inherit;
+//   border: 1px solid blue;
+//   padding: 8px;
+//   cursor: pointer;
+//   &:hover {
+//     background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
+//     color: black;
+//   }
+// `;
 
 class App extends Component {
   state = {
     persons: [
-      { id:'1asdfds', name: 'Aleko', age: 28},
-      { id:'2asfdc', name: 'Denzel', age: 26},
-      { id:'3awefw', name: 'Andrea', age: 29}
+      { id:'1', name: 'Aleko', age: 28},
+      { id:'2', name: 'Denzel', age: 26},
+      { id:'3', name: 'Andrea', age: 29}
     ],
     showPersons: false
   }
@@ -47,14 +61,18 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
+    // const style = {
+    //   backgroundColor: 'green',
+    //   color: 'white',
+    //   font: 'inherit',
+    //   border: '1px solid blue',
+    //   padding: '8px',
+    //   cursor: 'pointer',
+    //   ':hover': {
+    //     backgroundColor: 'lightgreen',
+    //     color: 'black'
+    //   }
+    // };
 
     let persons = null;
 
@@ -72,21 +90,33 @@ class App extends Component {
 
         </div> 
       );
-      style.backgroundColor = 'red';
+      // style.backgroundColor = 'red';
+      // style[':hover'] = {
+      //   backgroundColor: 'salmon',
+      //   color: 'black'
+      // }
+    }
+
+    const classes = [];
+
+    if (this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold');
     }
 
     return (
       <div className="App">
         <h1>Hi, I'm Aleko</h1>
-        <p>This is working</p>
-        <button 
-          style={style}
-          onClick={this.togglePersonsHandler}>Toggle Persons
-        </button>
+        <p className={classes.join(' ')}>This is working</p>
+        <div onClick={this.togglePersonsHandler}>
+          Toggle Persons
+        </div>
         {persons}
       </div>
     );
   }
 }
 
-export default App;
+export default App; 
